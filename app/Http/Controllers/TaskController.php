41a926bdcb17958d55a0ae8ec47496071d24f1a5
Makiese Morais <makiesse.dev@gmail.com>
2026-06-19 11:27:24 +0100

    Add Sanctum auth, API routes, and Project/Task controllers

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+
+    public function index()
+    {
+        return response()->json([
+            ['id' => 1, 'name' => 'Website Redesign'],
+            ['id' => 2, 'name' => 'Mobile App']
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json([
+            'message' => 'Project creaed',
+            'data' => $request->all()
+        ], 201);
+    }
+
+    public function show(string $id)
+    {
+        return response()->json(['id' => $id, 'name' => 'Website Redesign']);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        return response()->json(['message' => "Project {$id} updated"]);
+    }
+
+    public function destroy(string $id)
+    {
+        return response()->json(null, 204);
+    }
+}
